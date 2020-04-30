79dd5dee2881f05e4dc76926d90050e367173627
Jan Schiefer <jschiefer@users.noreply.github.com>
2020-04-30 04:03:45 +0000

    Some hello world stuff in Rust

--- CAN/bc/src/main.rs
+++ CAN/bc/src/main.rs
@@ -1,9 +1,24 @@
-use socketcan::CANSocket;
+use socketcan::{CANFrame, CANSocket};
+
+fn dumpframe(f: &CANFrame) {
+    print!("{:#x}: ", CANFrame::id(f));
+    let data = CANFrame::data(f);
+    println!("{:02x?}", data);
+    
+}
 
 fn main() {
     let a = CANSocket::open("can0");
     match a {
-        Ok(_) => println!("Open successful!"), 
-        Err(_) => println!("Open failed!")
+        Ok(desc) => {
+            loop {
+                let frame = CANSocket::read_frame(&desc);
+                match frame  {
+                    Ok(f) => dumpframe(&f),
+                    Err(_) => println!("Frame kaput"),
+                }
+            }
+        }
+        Err(_) => println!("Open failed!"),
     }
 }
